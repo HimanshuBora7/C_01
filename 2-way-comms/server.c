d8d3440ece326fc9a538cb10706eea980ca3555b
himanshubora7 <himanshubora972@gmail.com>
2026-05-05 02:38:21 +0530

    one way comm estb

--- 2-way-comms/server.c
+++ 2-way-comms/server.c
@@ -5,83 +5,92 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-int main(){
-    
-    //making endpoint or socket
-    int fd = socket(AF_UNIX,SOCK_STREAM,0);
-    if(fd == -1){
+int main()
+{
+
+    // making endpoint or socket
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd == -1)
+    {
         printf("failed");
-        return 1; 
+        return 1;
     }
 
-    //binding the socket to address so endpoint can have an address 
+    // binding the socket to address so endpoint can have an address
     struct sockaddr_un addr;
-    memset(&addr,0,sizeof(addr));
+    memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
     strcpy(addr.sun_path, "/tmp/mysocket");
-   
+
     unlink("/tmp/mysocket");
-    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
         perror("bind failed");
         return 1;
     }
- 
-    //listen part
 
-    
-   if (listen(fd, 5) == -1) {
-    perror("listen failed");
-    return 1;
-}
-printf("listening...\n");
-   int client_fd = accept(fd, NULL, NULL);
-if (client_fd == -1) {
-    perror("accept failed");
-    return 1;
-}
+    // listen part
 
-char buffer[100];
-fd_set readfds;
-
-while(1)
-{
-    FD_ZERO(&readfds);
-    FD_SET(0,&readfds);
-    FD_SET(client_fd, &readfds);
-
-    int maxfd= client_fd;
-     if (select(maxfd + 1, &readfds, NULL, NULL, NULL) == -1) {
-        perror("select failed");
-        break;
+    if (listen(fd, 5) == -1)
+    {
+        perror("listen failed");
+        return 1;
     }
-     if (FD_ISSET(client_fd, &readfds)) {
-        int n = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+    printf("listening...\n");
+    int client_fd = accept(fd, NULL, NULL);
+    if (client_fd == -1)
+    {
+        perror("accept failed");
+        return 1;
+    }
 
-        if (n <= 0) {
-            printf("Client disconnected\n");
+    char buffer[100];
+    fd_set readfds;
+
+    while (1)
+    {
+        FD_ZERO(&readfds);
+        FD_SET(0, &readfds);
+        FD_SET(client_fd, &readfds);
+
+        int maxfd = client_fd;
+        if (select(maxfd + 1, &readfds, NULL, NULL, NULL) == -1)
+        {
+            perror("select failed");
             break;
         }
+        if (FD_ISSET(client_fd, &readfds))
+        {
+            int n = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
 
-        buffer[n] = '\0';
-        printf("Client: %s", buffer);
-    }
-    if (FD_ISSET(0, &readfds)) {
-        printf("You: ");
-        fflush(stdout);
+            if (n <= 0)
+            {
+                printf("Client disconnected\n");
+                break;
+            }
 
-        if (fgets(buffer, sizeof(buffer), stdin) == NULL)
-            break;
+            buffer[n] = '\0';
+            printf("Client: %s", buffer);
+        }
+        if (FD_ISSET(0, &readfds))
+        {
+            printf("You: ");
+            fflush(stdout);
 
-        if (strncmp(buffer, "exit\n", 5) == 0)
-            break;
+            if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+                break;
 
-        if (send(client_fd, buffer, strlen(buffer), 0) == -1) {
-            perror("send failed");
-            break;
+            if (strncmp(buffer, "exit\n", 5) == 0)
+                break;
+
+            if (send(client_fd, buffer, strlen(buffer), 0) == -1)
+            {
+                perror("send failed");
+                break;
+            }
         }
     }
+    close(client_fd);
+    close(fd);
+    return 0;
 }
-close(client_fd);
-close(fd);
-return 0;
-} 
--- groupComms/client.c
+++ groupComms/client.c
@@ -1,14 +1,17 @@
-//client side of the program
+// client side of the program
 
-#include<stdio.h>
-#include<sys/socket.h>
+#include <stdio.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-int main(){
+#include <stdbool.h>
+
+int main()
+{
     // parameters passed, protocol we want, stream we want (tcp/udp), the IP layer we want to work beneath transport layer
-    // return fd (socker file descriptor) 
-    int socketFD =  socket(AF_INET,SOCK_STREAM,0);
+    // return fd (socker file descriptor)
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
 
     // we can use this fd to connect some remote socket
     // so we use connect function it takes fd , pointer to the address and length of address
@@ -16,38 +19,43 @@ int main(){
     // generic structure we pass as address
 
     struct sockaddr_in address;
-    char* ip = "142.250.68.206";
-    
-    //setting the properties of address
+    char *ip = "127.0.0.1";
 
-    //we can't directly assign the port address 
-    // this htons converts the port address to big endian format as tcp n udp expects port address in form of big endian
+    // setting the properties of address
 
-    address.sin_port = htons(80); // this port should be the port of server that is listening on the other side so there must be a process listening on same port on the address we gonna specify
+    // we can't directly assign the port address
+    //  this htons converts the port address to big endian format as tcp n udp expects port address in form of big endian
+
+    address.sin_port = htons(2000); // this port should be the port of server that is listening on the other side so there must be a process listening on same port on the address we gonna specify
     address.sin_family = AF_INET;
 
-    //ip address passsed here need to be converted into unsigned byte so to make this work we have a function if converts the ip into unsigned byte and put it in address property
-    inet_pton(AF_INET,ip,&address.sin_addr.s_addr);
+    // ip address passsed here need to be converted into unsigned byte so to make this work we have a function if converts the ip into unsigned byte and put it in address property
+    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
 
-    int result = connect(socketFD,&address,sizeof(address));
+    int result = connect(socketFD, &address, sizeof(address));
 
-    if(result == 0){
-        printf("connection established");
+    if (result == 0)
+    {
+        printf("connection established\n");
     }
+    memset(&address, 0, sizeof(address));
+    char *line = NULL;
 
-    char* message;
-    message = "GET \\ HTTP/1.1\r\nHost:google.com\r\n\r\n";
-    send(socketFD,message,strlen(message),0);
+    size_t linesize = 0;
+    printf("type a text to send or (exit)\n");
+    while (true)
+    {
+        ssize_t charCount = getline(&line, &linesize, stdin);
 
-    char* buffer[1024];
-    recv(socketFD,buffer,1024,0);
-
-    printf("response was %s\n ",buffer);
-
-
-
-
-
-
+        if (charCount > 0)
+        {
+            if (strcmp(line, "exit\n") == 0)
+            {
+                break;
+            }
+            ssize_t amountwasSent = send(socketFD, line, charCount, 0);
+        }
+    }
+    close(socketFD);
     return 0;
 }
